724cc93cf7e20535c0a10020a251f6a86a3ddeba
DaveSpivey <dspive1@gmail.com>
2016-05-14 16:53:29 -0700

    Add customer detail window

---
+++ public/js/details.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $('#show-details').click(showDetails);
+  $('#detail-close').click(closeDetails);
+
+})
+
+var savings = 700;
+var size = "5kW"
+
+setTimeout(function(){
+  $('#show-details').show();
+}, 4000)
+
+function showDetails() {
+  $('#show-details').hide();
+  $('#customer-details').show();
+  $('#savings-detail').append('<p>Estimated Annual Savings: $' + savings);
+  $('#savings-detail').append('<p>Recommended Solar Installation Size: ' + size);
+}
+
+function closeDetails() {
+  $('#customer-details').hide();
+  $('#show-details').show();
+}
